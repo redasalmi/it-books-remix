061ad0d7dc7fc72c8e89e6160b0bee7b6111432b
redasalmi <reda.salmi.elt@gmail.com>
2021-12-23 16:43:41 +0100

    added images conversion to avif format for index page

--- app/components/BooksList.tsx
+++ app/components/BooksList.tsx
@@ -18,10 +18,10 @@ interface BooksListProps {
 export default function BooksList({ books }: BooksListProps) {
   return (
     <div className='books'>
-      {books.map(({ title, image, isbn13 }) => (
+      {books.map(({ title, base64Image, isbn13 }) => (
         <div className='books-list-item' key={isbn13}>
           <Link to={`book/${isbn13}`} className='books-list-link'>
-            <img src={image} alt={title} className='books-list-img' />
+            <img src={base64Image} alt={title} className='books-list-img' />
             <p className='books-list-text'>{title}</p>
           </Link>
         </div>
